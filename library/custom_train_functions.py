5006013d6c6d373bede30f83e644fe25a8f40364
Cauldrath <bnjmnhanes@gmail.com>
2025-04-24 10:20:11 -0400

    custom noise schedule

--- library/custom_train_functions.py
+++ library/custom_train_functions.py
@@ -23,6 +23,11 @@ def prepare_scheduler_for_custom_training(noise_scheduler, device):
     all_snr = (alpha / sigma) ** 2
 
     noise_scheduler.all_snr = all_snr.to(device)
+    mu = 0
+    b = 0.75
+    laplace_weights = ((all_snr.log() - mu).abs() / -b).exp() / (2 * b)
+    laplace_weights /= laplace_weights.mean()
+    noise_scheduler.laplace_weights = laplace_weights.to(device)
 
 
 def fix_noise_scheduler_betas_for_zero_terminal_snr(noise_scheduler):
